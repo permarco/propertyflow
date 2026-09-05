5afc7682805f710136306452d2fdc7898988328d
Marco-Pertegato <marco.pertegato@e-laborate.ch>
2026-09-05 17:00:32 +0200

    init project

---
+++ src/main/java/com/pertegato/propertyflow/PropertyflowApplication.java
@@ -0,0 +1,13 @@
+package com.pertegato.propertyflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertyflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pertegato/propertyflow/PropertyflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pertegato.propertyflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
